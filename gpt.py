7a0c3da4af873ab24fd2db01135812f3ee0839c5
Srisurya Teja <121835313+SrisuryaTeja@users.noreply.github.com>
2025-04-21 18:26:34 +0530

    Updated Feed Forward.py

--- gpt.py
+++ gpt.py
@@ -104,7 +104,10 @@ class FeedForward(nn.Module):
 
     def __init__(self,n_embd):
         super().__init__()
-        self.net=nn.Sequential()
+        self.net=nn.Sequential(nn.Linear(n_embd,4*n_embd),
+                              nn.ReLU(),
+                              nn.Linear(4*n_embd,n_embd),
+                              nn.Droupout(dropout))
     
     def forward(self,x):
         return self.net(x)
